0cde5356df12fdbacf57442f5d8e7f704e18a011
dell <dell@Anuj>
2024-04-03 12:03:39 +0530

    by p

--- GitdemoD1/src/com/Demo.java
+++ GitdemoD1/src/com/Demo.java
@@ -2,6 +2,12 @@ package com;
 
 public class Demo 
 {
+	public void m1()
+	{
+		System.out.println("This is m1");
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("This is Git Demo");
 	}
